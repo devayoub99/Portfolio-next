a58eb6328cdcf580f6f6f19cbbcd4c83b75871e9
devayoub99 <admin@devayb.com>
2025-06-16 08:38:43 +0300

    Remove TailwindCSS and use my custom SCSS code.

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import "./main.css";
+// import "./globals.css";
+import "@/css/main.css";
 import PageHeader from "@/components/page-header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Ayoub - Portfolio website",
@@ -21,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* <div className="container mx-auto"></div> */}
+      <body className={``}>
         <PageHeader />
         {children}
       </body>
